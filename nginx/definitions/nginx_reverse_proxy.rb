4d1556469d9611e67cf4c1be87a66e4f4163bfd8
sasanadi ruka <didi@bacem.local>
2016-01-12 10:48:08 +0700

    fix variable 1

--- nginx/definitions/nginx_reverse_proxy.rb
+++ nginx/definitions/nginx_reverse_proxy.rb
@@ -1,8 +1,8 @@
 define :backend_nossl, :template => "backend_nossl.erb", :enable => true do
   include_recipe "nginx::service"
 
-  template "#{node[:nginx][:dir]}/sites-available/#{beno_bename}" do
-    Chef::Log.debug("Generating Nginx site template for #{beno_bename.inspect}")
+  template "#{node[:nginx][:dir]}/sites-available/#{node[:nginx][:backend_nossl][:beno_bename]}" do
+    Chef::Log.debug("Generating Nginx site template for backend_nossl")
     source params[:template]
     owner "root"
     group "root"
@@ -12,32 +12,32 @@ define :backend_nossl, :template => "backend_nossl.erb", :enable => true do
     end
     variables(
       :application => application,
-      :application_name => beno_bename,
+      :application_name => node[:nginx][:backend_nossl][:beno_bename],
       :params => params
     )
-    if File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{beno_bename}")
+    if File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{node[:nginx][:backend_nossl][:beno_bename]}")
       notifies :reload, "service[nginx]", :delayed
     end
   end
 
-  file "#{node[:nginx][:dir]}/sites-enabled/#{beno_bename}" do
+  file "#{node[:nginx][:dir]}/sites-enabled/#{node[:nginx][:backend_nossl][:beno_bename]}" do
     action :delete
     only_if do
-      File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{beno_bename}")
+      File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{node[:nginx][:backend_nossl][:beno_bename]}")
     end
   end
 
   if params[:enable]
-    execute "nxensite #{beno_bename}" do
-      command "/usr/sbin/nxensite #{beno_bename}"
+    execute "nxensite #{node[:nginx][:backend_nossl][:beno_bename]}" do
+      command "/usr/sbin/nxensite #{node[:nginx][:backend_nossl][:beno_bename]}"
       notifies :reload, "service[nginx]"
-      not_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{beno_bename}") end
+      not_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{node[:nginx][:backend_nossl][:beno_bename]}") end
     end
   else
-    execute "nxdissite #{beno_bename}" do
-      command "/usr/sbin/nxdissite #{beno_bename}"
+    execute "nxdissite #{node[:nginx][:backend_nossl][:beno_bename]}" do
+      command "/usr/sbin/nxdissite #{node[:nginx][:backend_nossl][:beno_bename]}"
       notifies :reload, "service[nginx]"
-      only_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{beno_bename}") end
+      only_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{node[:nginx][:backend_nossl][:beno_bename]}") end
     end
   end
 end
